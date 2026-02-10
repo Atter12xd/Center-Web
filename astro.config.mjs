73a42c46d0d7299bca4304241425715235a5a3aa
Atter12xd <1485275@senati.pe>
2026-02-09 19:31:02 -0500

    fs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,10 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://astros.zank.studio",
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "it", "es"],
+	},
 	vite: {
 		define: {
 			__DATE__: `'${new Date().toISOString()}'`,
